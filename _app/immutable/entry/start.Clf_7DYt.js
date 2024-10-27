2a17565e8fc7922d62f963febf2dba8e63ff3109
Grace <63696069+ClearCrystals@users.noreply.github.com>
2024-10-27 18:31:27 -0500

    Updates

---
+++ _app/immutable/entry/start.Clf_7DYt.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CAuSfbC9.js";export{t as start};
---
+++ _app/immutable/nodes/9.CGUl0jFC.js
@@ -0,0 +1,11 @@
+import{s as f,n as c}from"../chunks/scheduler.Cs0xm5t1.js";import{S as h,i as u,t as o,e as d,h as m,c as v,g as _,l as b,b as i,d as r}from"../chunks/index.DK9o55aT.js";function w(p){let s,n,a,e,x='<section class="w-full max-w-5xl text-center py-12"><h1 class="font-dream-avenue text-[4em] lg:text-[6em] tracking-wide mb-4 svelte-cekwkn">Projects</h1> <p class="text-lg lg:text-2xl max-w-3xl mx-auto">Explore some of my recent work and projects!</p></section>';return{c(){s=o("import "),n=o(base),a=o(` from '$app/paths';
+
+
+
+
+`),e=d("div"),e.innerHTML=x,this.h()},l(t){s=m(t,"import "),n=m(t,base),a=m(t,` from '$app/paths';
+
+
+
+
+`),e=v(t,"DIV",{class:!0,"data-svelte-h":!0}),_(e)!=="svelte-bzk00t"&&(e.innerHTML=x),this.h()},h(){b(e,"class","flex flex-col min-h-screen items-center bg-black text-white p-8")},m(t,l){i(t,s,l),i(t,n,l),i(t,a,l),i(t,e,l)},p:c,i:c,o:c,d(t){t&&(r(s),r(n),r(a),r(e))}}}function g(p){return[]}class y extends h{constructor(s){super(),u(this,s,g,w,f,{})}}export{y as component};
